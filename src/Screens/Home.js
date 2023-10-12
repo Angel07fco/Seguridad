cf7f392b9f34d0aed4c2b7516799d45076fa16c8
Angel Fco <angel02fco@gmail.com>
2023-10-11 23:22:09 -0600

    add files

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,7 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react';
+import CryptoJS from 'crypto-js';
 
-export default function Home() {
+function Home() {
+  const [texto, setTexto] = useState('');
+  const [accion, setAccion] = useState('cifrar');
+  const [resultado, setResultado] = useState('');
+
+  const cifrar = (texto) => {
+    var textoCifrado = CryptoJS.AES.encrypt(texto, '@angelscript').toString();
+    return textoCifrado;
+  }
+
+  const descifrar = (texto) => {
+    var bytes = CryptoJS.AES.decrypt(texto, '@angelscript');
+    var textoDescifrado = bytes.toString(CryptoJS.enc.Utf8);
+    return textoDescifrado;
+  }
+
+  const handleTextoChange = (e) => {
+    setTexto(e.target.value);
+  };
+
+  const handleAccionChange = (e) => {
+    setAccion(e.target.value);
+  };
+
+  const handleObtenerResultado = () => {
+    // Ejemplo de implementación:
+    if (accion === 'cifrar') {
+      var tcifrado = cifrar(texto);
+      setResultado(tcifrado)
+    } else if (accion === 'descifrar') {
+      var tdescifrado = descifrar(texto);
+      setResultado(tdescifrado)
+    }
+  };
+
   return (
-    <div>Home</div>
-  )
+    <div className="App">
+      <h1>Cifrado y Descifrado</h1>
+      <div>
+        <label htmlFor="texto">Texto:</label>
+        <input
+          type="text"
+          id="texto"
+          value={texto}
+          onChange={handleTextoChange}
+        />
+      </div>
+      <div>
+        <input
+          type="radio"
+          id="cifrar"
+          name="accion"
+          value="cifrar"
+          checked={accion === 'cifrar'}
+          onChange={handleAccionChange}
+        />
+        <label htmlFor="cifrar">Cifrar</label>
+        <input
+          type="radio"
+          id="descifrar"
+          name="accion"
+          value="descifrar"
+          checked={accion === 'descifrar'}
+          onChange={handleAccionChange}
+        />
+        <label htmlFor="descifrar">Descifrar</label>
+      </div>
+      <button onClick={handleObtenerResultado}>Obtener</button>
+      <div>
+        <label htmlFor="resultado">Resultado:</label>
+        <textarea
+          id="resultado"
+          rows="4"
+          cols="50"
+          value={resultado}
+          readOnly
+        ></textarea>
+      </div>
+    </div>
+  );
 }
+
+export default Home;
